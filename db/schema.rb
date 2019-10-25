86092e4f7f3ef268eac4bee9f1b07738d201ea71
kt <cyp_k_1701@yahoo.co.jp>
2019-10-25 17:59:19 +0900

    Initialize repository

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_021839) do
+ActiveRecord::Schema.define(version: 2019_10_24_135625) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
+    t.string "password"
   end
 
 end
